280528cbde9e15bc7db3a6f68b52a7e9e565de6b
scottybean <60233939+scottybean@users.noreply.github.com>
2020-01-27 12:44:47 -0500

    Add files via upload
    I could not get my javascript code to work

---
+++ Basic_HTML_and_JavaScript1/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+<script>
+  function showAlert() {
+    alert ("Hello world!");
+  }
+  </script>
+  
+<script>
+	function My_First_Function() {
+		var string = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementByID("Irish").innerHTML =
+		result;
+	}
+</script>
