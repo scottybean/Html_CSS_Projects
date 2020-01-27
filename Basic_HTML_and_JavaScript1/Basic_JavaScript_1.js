d9d98fcbc8f3e205d5d1db3cc2398ceac14afd9b
scottybean <60233939+scottybean@users.noreply.github.com>
2020-01-27 15:09:24 -0500

    Add files via upload
    With the sunset

--- Basic_HTML_and_JavaScript1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript1/Basic_JavaScript_1.js
@@ -1,14 +1,9 @@
-<script>
   function showAlert() {
     alert ("Hello world!");
   }
-  </script>
   
-<script>
 	function My_First_Function() {
-		var string = "Kiss me, I'm Irish!";
+		var String = "Kiss me, I'm Irish!";
 		var result = String.fontcolor("green");
-		document.getElementByID("Irish").innerHTML =
-		result;
+		document.getElementById("Irish").innerHTML = result;
 	}
-</script>
